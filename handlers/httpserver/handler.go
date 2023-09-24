5b64891242ba355597b82ba9165c5ec77f4a1c3f
vano2903 <davidevanoncini2003@gmail.com>
2023-09-25 01:53:59 +0200

    feat: add html pages routes and static files routes

--- handlers/httpserver/handler.go
+++ handlers/httpserver/handler.go
@@ -26,8 +26,30 @@ func NewHttpHandler(e *echo.Echo, c *controller.Controller, l *logrus.Logger) *h
 
 // Registers only the routes and links functions
 func (h *httpHandler) RegisterRoutes() {
+	h.e.Static("/static", "static")
+
+	pages := h.e.Group("")
+	pages.GET("/", h.IndexPage)
+	pages.GET("/attempt", h.NewAttemptPage)
+	pages.GET("/attempt/:code", h.AttemptInfoPage)
+
 	api := h.e.Group("/api/v1")
 	attempt := api.Group("/attempt")
 	attempt.POST("/new", h.Upload)
 	attempt.GET("/info/:code", h.GetAttemptInfo)
+
+	leaderboard := api.Group("/leaderboard")
+	leaderboard.GET("/list", h.GetLeaderboard)
+}
+
+func (h *httpHandler) IndexPage(c echo.Context) error {
+	return c.File("pages/index.html")
+}
+
+func (h *httpHandler) NewAttemptPage(c echo.Context) error {
+	return c.File("pages/attempt.html")
+}
+
+func (h *httpHandler) AttemptInfoPage(c echo.Context) error {
+	return c.File("pages/attemptInfo.html")
 }
