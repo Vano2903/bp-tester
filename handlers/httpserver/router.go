1bf1999ec0142a1d4f97f9dfa736188a4d543c56
vano2903 <davidevanoncini2003@gmail.com>
2023-10-04 18:14:52 +0200

    fix: use echo as http server instead of wrapping it around an http server

--- handlers/httpserver/router.go
+++ handlers/httpserver/router.go
@@ -32,6 +32,14 @@ func InitRouter(e *echo.Echo, l *logrus.Logger, controller *controller.Controlle
 		},
 	}))
 	// e.Use(middleware.Recover())
+	// e.Use(middleware.RecoverWithConfig(middleware.RecoverConfig{
+	// 	StackSize:         4 << 10, // 4 KB
+	// 	DisableStackAll:   true,
+	// 	DisablePrintStack: false,
+	// 	LogLevel:          1,
+	// }))
+
+	// e.Use(middleware.SecureWithConfig(middleware.DefaultSecureConfig))
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		Skipper:      middleware.DefaultSkipper,
@@ -40,8 +48,11 @@ func InitRouter(e *echo.Echo, l *logrus.Logger, controller *controller.Controlle
 	}))
 
 	echo.NotFoundHandler = func(c echo.Context) error {
-		return respError(c, http.StatusNotFound, "invalid endpoint", "")
+		return respError(c, http.StatusNotFound, "invalid endpoint")
 	}
+
+	e.HideBanner = true
+	e.HidePort = true
 
 	httpHandler := NewHttpHandler(e, controller, l)
 	httpHandler.RegisterRoutes()
@@ -54,28 +65,24 @@ func StartRouter(ctx context.Context, h *httpHandler, conf *config.Config, ID in
 			h.l.Errorf("router panic, recovering: \nerror: %v\n\nstack: %s", r, string(debug.Stack()))
 		}
 		if ctx.Err() == nil {
+			h.l.Info("router recovered, restarting")
 			RoutineMonitor <- ID
 		} else {
 			h.l.Info("API not restarting, context was canceled")
 		}
 	}()
 
-	h.l.Info(">>> STARTING HTTP SERVER >>>")
-
-	s := http.Server{
-		Addr:    "0.0.0.0:" + conf.HTTP.Port,
-		Handler: h.e,
-	}
-
 	go func() {
 		<-ctx.Done()
-		if err := s.Shutdown(ctx); err != nil {
+		if err := h.e.Shutdown(ctx); err != nil {
 			h.l.Errorf("error stopping http server: %v", err)
 		}
 	}()
 
-	h.l.Infof(">>> STARTING API ON: %s >>>", s.Addr)
-	err := s.ListenAndServe()
+	addr := "0.0.0.0:" + conf.HTTP.Port
+	h.l.Infof(">>> STARTING API ON: %s >>>", addr)
+	// var err error
+	err := h.e.Start(addr)
 	if err != nil {
 		if ctx.Err() != nil {
 			h.l.Info(">>> STOPPING SERVER >>>")
@@ -85,6 +92,5 @@ func StartRouter(ctx context.Context, h *httpHandler, conf *config.Config, ID in
 		} else {
 			h.l.Errorf(">>> ERROR STARTING SERVER: %v", err.Error())
 		}
-		return
 	}
 }
