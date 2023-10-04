a539667883bb2bffcb440d16bbc1c6b2c0e84a4b
vano2903 <davidevanoncini2003@gmail.com>
2023-10-04 18:19:22 +0200

    feat: add token and user errors

--- controller/errors.go
+++ controller/errors.go
@@ -6,7 +6,13 @@ var (
 	ErrEmtpySource   = errors.New("empty source")
 	ErrSourceTooLong = errors.New("source too long")
 
-	ErrQeueuFull     = errors.New("queue is full")
+	ErrQeueuFull = errors.New("queue is full")
 
 	ErrImageNotCompiledCorrectly = errors.New("image not compiled correctly")
+
+	ErrTokenExpired = errors.New("token expired")
+	ErrInvalidToken = errors.New("invalid token")
+
+	ErrUserNotFound       = errors.New("user not found")
+	ErrInvalidCredentials = errors.New("invalid credentials")
 )
