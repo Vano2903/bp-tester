dce32c26c690ff256fda55317b5074eb381f8544
vano2903 <davidevanoncini2003@gmail.com>
2023-10-04 18:16:24 +0200

    feat: add route for user and token in api

--- handlers/httpserver/handler.go
+++ handlers/httpserver/handler.go
@@ -6,6 +6,11 @@ import (
 	"github.com/vano2903/bp-tester/controller"
 )
 
+const (
+	accessTokenCookieName  = "access-token"
+	refreshTokenCookieName = "refresh-token"
+)
+
 type (
 	httpHandler struct {
 		e          *echo.Echo
@@ -30,10 +35,19 @@ func (h *httpHandler) RegisterRoutes() {
 
 	pages := h.e.Group("")
 	pages.GET("/", h.IndexPage)
+	pages.GET("/register", h.RegisterPage)
+	pages.GET("/login", h.LoginPage)
 	pages.GET("/attempt", h.NewAttemptPage)
 	pages.GET("/attempt/:code", h.AttemptInfoPage)
 
 	api := h.e.Group("/api/v1")
+	user := api.Group("/user")
+	user.POST("/register", h.Register)
+	user.POST("/login", h.Login)
+
+	tokens := api.Group("/tokens")
+	tokens.GET("/refresh", h.RefreshTokens)
+
 	attempt := api.Group("/attempt")
 	attempt.POST("/new", h.Upload)
 	attempt.GET("/info/:code", h.GetAttemptInfo)
@@ -47,7 +61,17 @@ func (h *httpHandler) IndexPage(c echo.Context) error {
 }
 
 func (h *httpHandler) NewAttemptPage(c echo.Context) error {
+	//if the user is not logged it it can still do an attempt
+	//but it will not be considered in the leaderboard
 	return c.File("pages/attempt.html")
+}
+
+func (h *httpHandler) RegisterPage(c echo.Context) error {
+	return c.File("pages/register.html")
+}
+
+func (h *httpHandler) LoginPage(c echo.Context) error {
+	return c.File("pages/login.html")
 }
 
 func (h *httpHandler) AttemptInfoPage(c echo.Context) error {
