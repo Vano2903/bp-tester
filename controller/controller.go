0d83ef54ab38343600c0547b6d3fb2b25b3446a3
vano2903 <davidevanoncini2003@gmail.com>
2023-10-04 18:18:59 +0200

    feat: add user and token repos

--- controller/controller.go
+++ controller/controller.go
@@ -12,17 +12,26 @@ import (
 )
 
 type Controller struct {
-	buildDir      string
-	config        *config.Config
-	lastAttemptID int
-	attemptRepo   repo.AttemptRepoer
-	executionRepo repo.ExecutionRepoer
-	buildQueue    chan *model.Attempt
-	l             *logrus.Logger
-	cli           *client.Client
+	buildDir         string
+	config           *config.Config
+	attemptRepo      repo.AttemptRepoer
+	executionRepo    repo.ExecutionRepoer
+	userRepo         repo.UserRepoer
+	accessTokenRepo  repo.AccessTokenRepoer
+	refreshTokenRepo repo.RefreshTokenRepoer
+	buildQueue       chan *model.Attempt
+	l                *logrus.Logger
+	cli              *client.Client
 }
 
-func NewController(l *logrus.Logger, config *config.Config, attempRepo repo.AttemptRepoer, executionRepo repo.ExecutionRepoer, ctx context.Context) (*Controller, error) {
+func NewController(ctx context.Context,
+	l *logrus.Logger,
+	config *config.Config,
+	attempRepo repo.AttemptRepoer,
+	executionRepo repo.ExecutionRepoer,
+	userRepo repo.UserRepoer,
+	accessTokenRepo repo.AccessTokenRepoer,
+	refreshTokenRepo repo.RefreshTokenRepoer) (*Controller, error) {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		return nil, err
@@ -30,19 +39,20 @@ func NewController(l *logrus.Logger, config *config.Config, attempRepo repo.Atte
 	cli.NegotiateAPIVersion(ctx)
 
 	return &Controller{
-		l:             l,
-		buildDir:      "./build",
-		config:        config,
-		cli:           cli,
-		attemptRepo:   attempRepo,
-		executionRepo: executionRepo,
-		lastAttemptID: 0, //i know it's 0 by default, but i want to be explicit
-		buildQueue:    make(chan *model.Attempt, 100),
+		l:                l,
+		buildDir:         "./build",
+		config:           config,
+		cli:              cli,
+		attemptRepo:      attempRepo,
+		executionRepo:    executionRepo,
+		userRepo:         userRepo,
+		accessTokenRepo:  accessTokenRepo,
+		refreshTokenRepo: refreshTokenRepo,
+		buildQueue:       make(chan *model.Attempt, 100),
 	}, nil
 }
 
 func (c *Controller) init() error {
-	//create build directory
 	if err := os.MkdirAll(c.buildDir, 0777); err != nil {
 		return err
 	}
